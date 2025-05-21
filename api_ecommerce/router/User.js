fefd458a7945ff88d6674d53f3c32c91ec580460
Julianfr23 <juliandavid199@gmail.com>
2025-05-20 23:26:08 -0500

    Estructura Backend

---
+++ api_ecommerce/router/User.js
@@ -0,0 +1,14 @@
+import routerx from 'express-promise-router'
+import usercontroller from '../controllers/UserController'
+import auth from '../middlewares/auth'
+
+const router = routerx();
+// http://localhost:3000/api/users/register
+
+router.post("/register",usercontroller.register);
+router.put("/update",usercontroller.update);
+router.get("/list",auth.verifyAdmin,usercontroller.list);
+router.post("/login",usercontroller.login);
+router.delete("/delete",usercontroller.remove);
+
+export default router;
---
+++ ecommerce/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+declare var $:any;
+declare function HOMEINITTEMPLATE([]):any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ecommerce';
+  ngOnInit():void {
+    setTimeout(() => {
+      HOMEINITTEMPLATE($);
+    }, 50);
+  }
+}
