dfe8e7e04bacbecad39fbca614c7eb1e85b493bb
Julianfr23 <juliandavid199@gmail.com>
2025-06-01 20:26:23 -0500

    Modulo Cupones Final

--- api_ecommerce/router/index.js
+++ api_ecommerce/router/index.js
@@ -3,7 +3,7 @@ import User from './User'
 import Categorie from './Categorie'
 import Product from './Product'
 import Slider from './Slider'
-import Cupone from '../models/Cupone'
+import Cupone from './Cupone'
 
 const router = routerx();
 router.use('/users', User);
